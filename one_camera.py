a9a86d0760b881885163626caf3ed3b2b291d971
Eloy Moreno Garcia <eloymorenogarcia@MacBook-Air-de-Eloy.local>
2017-09-20 12:53:31 +0200

    2

--- one_camera.py
+++ one_camera.py
@@ -2,8 +2,14 @@ import math
 import scipy.misc
 import matplotlib.pyplot as plt
 import numpy as np
-import skimage.exposure
-from skimage.measure import compare_ssim as ssim
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+def get_image():
+ # read is the easiest way to get a full image out of a VideoCapture object.
+ retval, im = camera.read()
+ return im
 
 def base_convert(i, b):
     result = []
@@ -18,13 +24,13 @@ def hadamard(size,vector):
 
     # size is 2 power to order
     order = int(math.log(size, 2))
-    vector = vector[::-1]
+   
     v_m = [[1,1,1,-1],[1,1,-1,1],[1,-1,1,1],[-1,1,1,1]]
     h_m = np.array([[1]])
-    
+
     for i in xrange(0,order):
-        h_m1 = np.concatenate((v_m[vector[i]][0]*h_m,v_m[vector[i]][2]*h_m))
-        h_m2 = np.concatenate((v_m[vector[i]][1]*h_m,v_m[vector[i]][3]*h_m))
+        h_m1 = np.concatenate((v_m[vector[i]][0]*h_m,v_m[vector[i]][1]*h_m))
+        h_m2 = np.concatenate((v_m[vector[i]][2]*h_m,v_m[vector[i]][3]*h_m))
         h_m = np.concatenate((h_m1,h_m2),1)
     
     return h_m
@@ -35,43 +41,37 @@ def generate_hadamard(n):
         padding = np.zeros(7-len(c),dtype="int")      
         c = np.concatenate((padding,c))      
         had = hadamard(128,c)
-        #had = had[:,:]==1
-        #had = 1*had
+        had = had[:,:]==1
+        had = 1*had
        
         matrix_vector.append(had)
 
     return matrix_vector
 
-face = scipy.misc.face()
-face = face[:,:,0]
-face = scipy.misc.imresize(face,[128,128])
-face = face.astype("float64")
-
-nn=4**7
-M=15000
-hadamard_masks = generate_hadamard(nn)
+hadamard_masks = generate_hadamard(4**7)
 np.random.shuffle(hadamard_masks)
-
-res = np.ones(face.shape)
 num=1
-for i in range(0,M):
-    mask = hadamard_masks[i]==1
-    mask = mask*1
-    masked = face*hadamard_masks[i]
+
+im_vec=[]
+imM = scipy.misc.imresize(get_image(),[128,128])
+for i in range(0,1000):
+    im = scipy.misc.imresize(get_image(),[128,128])
+    im_vec.append(im)
+
+res = np.zeros(imM.shape)
+
+
+for i in range(0,len(hadamard_masks)):
+    masked = im_vec[i]*hadamard_masks[i]
     intensity = masked.sum(dtype="float64")
-    pixels=hadamard_masks[i].sum(dtype="float64")
-    
-    res += (((intensity))*hadamard_masks[i])
+    pixels=m.sum(dtype="float64")
+    res += (((intensity/pixels)/4**7)*m)
     num+=1
-
-res = skimage.exposure.rescale_intensity(res,out_range=(0, 255))
-
-print ssim(res,face)
 
 fig = plt.figure()
 a=fig.add_subplot(1, 2, 1)
 plt.gray()
-plt.imshow(face)
+plt.imshow(imM)
 a=fig.add_subplot(1, 2, 2)
 plt.imshow(res)
 plt.show()
---
+++ test.py
@@ -0,0 +1,24 @@
+import cv2
+import matplotlib.pyplot as plt
+import scipy.misc
+import time
+
+camera = cv2.VideoCapture(0)
+ 
+# Captures a single image from the camera and returns it in PIL format
+def get_image():
+ # read is the easiest way to get a full image out of a VideoCapture object.
+ retval, im = camera.read()
+ return im
+
+im_vec=[]
+
+tic = time.clock()
+
+for i in range(0,1000):
+    im = scipy.misc.imresize(get_image(),[128,128])
+    im_vec.append(im)
+
+toc = time.clock()
+
+print (toc-tic)
