0887d73bee3f6b3dd4a1db50cd94ed296acded24
Eloy Moreno Garcia <eloymorenogarcia@MacBook-Air-de-Eloy.local>
2017-10-25 12:38:38 +0200

    main

--- main.py
+++ main.py
@@ -6,86 +6,153 @@ import math
 import numpy as np
 import scipy.misc
 import matplotlib.pyplot as plt
+import scipy.fftpack as spfft
 import cvxpy as cvx
+from pylbfgs import owlqn
+
+
+class Simulator(object):
+    """
+    Generate a simulated samples vector
+    """
+    def __init__(self, num_samples, image):
+        self.num_samples = num_samples
+        self.image = image
+
+    def hadamard_samples(self):
+        return self.__auxiliar_function("h")
+
+    def random_samples(self):
+        return self.__auxiliar_function("r")
+
+    def __auxiliar_function(self, t):
+        m = Masks()
+        np.random.seed(1)
+        if t == "r":
+            masks = m.generate_random(self.num_samples, self.image.shape[0])
+        elif t == "h":
+            masks = m.generate_hadamard(self.num_samples, self.image.shape[0])
+            np.random.shuffle(masks)
+        samples = []
+        for i in masks:
+            masked = i * self.image
+            samples.append(np.sum(masked))
+        return samples
+
 
 class One(object):
-    def __init__(self,image):
-        self.image = image
-    def reconstruction(self, method='', image, samples)
-        nx,ny = image.shape
-        m = Masks(nx)
-        if method = 'hadamard':
-            hadamard_masks = m.generate_hadamard(nx*ny)
+    def reconstruction(self, samples, image_size, method=''):
+        m = Masks()
+        np.random.seed(1)
+        self.image_size = image_size
+        self.samples = samples
+        if method == 'hadamard':
+            hadamard_masks = m.generate_hadamard(len(samples), image_size)
             np.random.shuffle(hadamard_masks)
-            num=1
-
-            im_vec=[]
-            imM = scipy.misc.imresize(get_image(), [128, 128])
-            for i in range(0, 1000):
-                im = scipy.misc.imresize(get_image(), [128, 128])
-                im_vec.append(im)
-
-            res = np.zeros(imM.shape)
-
-
-            for i in range(0, len(hadamard_masks)):
-                masked = im_vec[i]*hadamard_masks[i]
-                intensity = masked.sum(dtype="float64")
-                pixels=m.sum(dtype="float64")
-                res += (((intensity/pixels)/4**7)*m)
-                num+=1
-        
-
-        elif method = 'fourier':
-
+            res = np.zeros([image_size, image_size])
+            for i in range(0, len(samples)):
+                res += hadamard_masks[i] * samples[i]
+        if method == 'fourier':
+            random_masks = m.generate_random(len(samples), image_size)
+            random_masks_formated = []
+            for i in random_masks:
+                random_masks_formated.append(i.T.flatten())
             A = np.kron(
-            spfft.idct(np.identity(nx), norm='ortho', axis=0),
-            spfft.idct(np.identity(ny), norm='ortho', axis=0)
-            )
-            m = Masks()
-            mask_vec_raw = m.generate_random(samples)
-            mask_vec =[]
-            for i in range(0,nx*ny):
-                mask_vec.append(mask_vec[i].T.flatten())
-            mask_vec_formated = np.expand_dims(mask_vec, axis=1)
-            A = np.dot(mask_vec_formated,A)
-
-            for i in range(0,M):
-                mask = mask_vec[i]
-                masked = mask*image
-                intensity = np.sum(masked)
-                intensity_vec.append(intensity)
-
-            # do L1 optimization
-            AX = A[0:M,:]
-            vx = cvx.Variable(nx * ny)
+                spfft.idct(np.identity(image_size), norm='ortho', axis=0),
+                spfft.idct(np.identity(image_size), norm='ortho', axis=0))
+            A = np.dot(random_masks_formated, A)
+            vx = cvx.Variable(image_size * image_size)
             objective = cvx.Minimize(cvx.norm(vx, 1))
-            constraints = [AX*vx == intensity_vec]
+            constraints = [A * vx == samples]
             prob = cvx.Problem(objective, constraints)
-            result = prob.solve()
+            result = prob.solve(verbose=True)
             Xat2 = np.array(vx.value).squeeze()
-            Xat = Xat2.reshape(nx, ny).T
-            Xa = self.idct2(Xat)
-            Xa = Xa.astype("float64")
-            return Xa
+            Xat = Xat2.reshape(image_size, image_size).T
+            res = self.__idct2(Xat)
+        if method == 'fourier_optim':
+            random_masks = m.generate_random(len(samples), image_size)
+            random_masks_formated = []
+            for i in random_masks:
+                random_masks_formated.append(i.T.flatten())
+            self.random_masks = random_masks_formated
+            Xat2 = owlqn(image_size * image_size, self.__evaluate,
+                         self.__progress, 10000)
+            Xat = Xat2.reshape(image_size, image_size).T  # stack columns
+            Xa = self.__idct2(Xat)
+            res = Xa.astype('uint8')
+        return res
+
     @staticmethod
-    def idct2(x):
-        return spfft.idct(spfft.idct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
+    def __idct2(x):
+        return spfft.idct(
+            spfft.idct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
+
+    @staticmethod
+    def __dct2(x):
+        return spfft.dct(
+            spfft.dct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
+
+    def __evaluate(self, x, g, step):
+        """An in-memory evaluation callback."""
+
+        # we want to return two things:
+        # (1) the norm squared of the residuals, sum((Ax-b).^2), and
+        # (2) the gradient 2*A'(Ax-b)
+
+        # expand x columns-first
+        x2 = x.reshape((self.image_size, self.image_size)).T
+
+        # Ax is just the inverse 2D dct of x2
+        Ax2 = self.__idct2(x2)
+
+        Ax = np.dot(self.random_masks, Ax2.T.flatten())
+
+        # calculate the residual Ax-b and its 2-norm squared
+
+        Axb = Ax - self.samples
+
+        fx = np.sum(np.power(Axb, 2))
+
+        Axb2 = np.zeros(x2.shape, dtype="float64")
+        for a in range(0, len(self.random_masks)):
+            Axb2 += self.random_masks[a].reshape(x2.shape).T * Axb[a]
+
+        # A'(Ax-b) is just the 2D dct of Axb2
+        AtAxb2 = 2 * self.__dct2(Axb2)
+        AtAxb = AtAxb2.T.reshape(x.shape)  # stack columns
+        # copy over the gradient vector
+
+        np.copyto(g, AtAxb)
+
+        return fx
+
+    @staticmethod
+    def __progress(x, g, fx, xnorm, gnorm, step, k, ls):
+        # Print variables to screen or file or whatever. Return zero to
+        # continue algorithm; non-zero will halt execution.
+        if gnorm < 5:
+            a = 1
+        else:
+            a = 0
+        return a
+
 
 class Image(object):
     def __init__(self):
         """
         Constructor
         """
-    def return_image(self, size = 64, path = ''):
+
+    def return_image(self, size=64, path=''):
         """
         Return image
         """
-        if path =='':
+        if path == '':
             r_image = self.__normalize_image(scipy.misc.face(), size)
         else:
             r_image = self.__normalize_image(plt.imread(path), size)
         return r_image
+
     @staticmethod
     def __normalize_image(image, size):
         n_image = image[:, :, 1]
@@ -93,30 +160,44 @@ class Image(object):
         n_image = n_image.astype("float64")
         return n_image
 
+
 class Masks(object):
     """
     todo
     """
 
-    def __init__(self, image_size):
-        """Constructor"""
+    def generate_hadamard(self, number, size):
+        """Generate a n hadamard matrix vector"""
+        matrix_vector = []
+        for i in range(0, number):
+            hadamard_matrix = self.__hadamard(i, size)
+            matrix_vector.append(hadamard_matrix)
+        return matrix_vector
 
-        self.image_size = image_size
+    def generate_random(self, number, size):
+        """Generate a n random matrix vector"""
+        matrix_vector = []
+        np.random.seed(1)
+        for _ in range(0, number):
+            random_matrix = (np.random.rand(size, size) < 0.5) * 1
+            matrix_vector.append(random_matrix)
+        return matrix_vector
 
-    def hadamard(self, id_num):
+    def __hadamard(self, id_num, size):
         """
         Private Method
         Return a hadamard matrix
         """
-        order = int(math.log(self.image_size, 2))
+        order = int(math.log(size, 2))
         matrix_code = self.__base_convert(id_num, 4)
-        padding = np.zeros(7 - len(matrix_code), dtype="int")
+        padding = np.zeros(
+            int(math.log(size**2, 4)) - len(matrix_code), dtype="int")
         vector = np.concatenate((padding, matrix_code))
         vector = vector[::-1]
         v_m = [[1, 1, 1, -1], [1, 1, -1, 1], [1, -1, 1, 1], [-1, 1, 1, 1]]
         h_m = np.array([[1]])
 
-        for i in xrange(0, order):
+        for i in range(0, order):
             h_m1 = np.concatenate((v_m[vector[i]][0] * h_m,
                                    v_m[vector[i]][2] * h_m))
             h_m2 = np.concatenate((v_m[vector[i]][1] * h_m,
@@ -124,22 +205,6 @@ class Masks(object):
             h_m = np.concatenate((h_m1, h_m2), 1)
 
         return h_m
-
-    def generate_hadamard(self, number):
-        """Generate a n hadamard matrix vector"""
-        matrix_vector = []
-        for i in xrange(0, number):
-            hadamard_matrix = self.hadamard(i)
-            matrix_vector.append(hadamard_matrix)
-        return matrix_vector
-
-    def generate_random(self, number):
-        """Generate a n random matrix vector"""
-        matrix_vector = []
-        for _ in xrange(0, number):
-            random_matrix = (np.random.rand(self.image_size, self.image_size) < 0.5)*1
-            matrix_vector.append(random_matrix)
-        return matrix_vector
 
     @staticmethod
     def __base_convert(number, base):
@@ -151,3 +216,10 @@ class Masks(object):
             result.insert(0, number % base)
             number = number // base
         return result
+
+
+def imshow(im):
+    plt.figure()
+    plt.gray()
+    plt.imshow(im)
+    plt.show()
---
+++ test_main.py
@@ -0,0 +1,20 @@
+import main
+import matplotlib.pyplot as plt
+from skimage.measure import compare_ssim as ssim
+
+def imshow(im):
+    plt.figure()
+    plt.gray()
+    plt.imshow(im)
+    plt.show()
+
+i = main.Image()
+im = i.return_image()
+s = main.Simulator(500,im)
+o = main.One()
+res = o.reconstruction(s.hadamard_samples(),im.shape[0],method='fourier_optim')
+imshow(res)
+res = o.reconstruction(s.hadamard_samples(),im.shape[0],method='hadamard')
+imshow(res)
+res = o.reconstruction(s.random_samples(),im.shape[0],method='fourier')
+imshow(res)
